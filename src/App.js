ed6a399ad4351fd22eaab4d86b9b950eed1aea8d
smurf1es <pahuger_puruhita_27rpl@student.smktelkom-mlg.sch.id>
2020-05-07 12:56:12 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Table, Button } from "reactstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Title</th>
+            <th>Rating</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Book title</td>
+            <td>2.5</td>
+            <td>
+              <Button color="success" size="sm">
+                Edit
+              </Button>
+              <Button color="danger" size="sm">
+                Delete
+              </Button>
+            </td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
